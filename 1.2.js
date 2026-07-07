0bd8771fd4f864205ef8bc72038e1cb6b1f6b5a7
CaioFZ19 <caiofabiofd10@gmail.com>
2026-07-06 22:16:21 -0300

    Delete 1.2.js

--- 1.2.js
+++
@@ -1,8 +0,0 @@
-import promptSync from 'prompt-sync'
-const prompt = promptSync()
-
-const ano = 2026
-let idade = prompt ("Qual a sua idade? ")
-let nascimento = Number (ano - idade)
-
-console.log (`Você nasceu em ${nascimento}`)
